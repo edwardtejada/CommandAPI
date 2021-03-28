cfc3b441643a5d26505ccc2ea2d7585f80bca29c
Edward M. Tejada Dipre <etejada@htnal.local>
2021-03-28 15:44:28 -0400

    modify the azure-pipeline file

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -12,8 +12,6 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-
-        //Random Change
         private readonly ICommandAPIRepo _repository;
         private readonly IMapper _mapper;
 
